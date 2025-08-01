81fd0afb454939336a2a345e5df3d7eec783dbf2
Juan Pedro Mellado <mellado@gmx.de>
2025-08-01 11:20:33 +0200

    First serial implementation of matix splitting.

--- src/utils/thomas3_split.f90
+++ src/utils/thomas3_split.f90
@@ -9,26 +9,40 @@ module Thomas3_Split
     implicit none
     private
 
-    public :: Thomas3_Split_Initialize
-    public :: Thomas3_Split_Solve
+    public :: Thomas3_Split_Initialize_Global
+    public :: Thomas3_Split_Solve_Global
 
     type, public :: matrix_split_dt
         integer :: np                           ! number of splitting points
+        logical :: periodic = .false.
         integer(wi), allocatable :: points(:)   ! sequence of splitting points in ascending order
-        real(wp), allocatable :: alpha(:, :)
         real(wp), allocatable :: z(:, :)
+        real(wp), allocatable :: alpha(:, :)
+        real(wp), allocatable :: beta(:)
+        real(wp), allocatable :: gamma(:)
     end type matrix_split_dt
+
+    type, public :: thomas3_split_dt
+        integer :: block
+        integer(wi) :: pmin, pmax
+        logical :: periodic = .false.
+        real(wp), allocatable :: a(:), b(:), c(:)
+        real(wp), allocatable :: z(:, :)
+        real(wp), allocatable :: alpha(:, :)
+        real(wp), allocatable :: beta(:)
+        real(wp), allocatable :: gamma(:)
+    end type thomas3_split_dt
 
 contains
     !########################################################################
     !########################################################################
-    subroutine Thomas3_Split_Initialize(a, b, c, points, split)
+    subroutine Thomas3_Split_Initialize_Global(a, b, c, points, split)
         real(wp), intent(inout) :: a(:), b(:), c(:)
         integer(wi), intent(in) :: points(:)   ! sequence of splitting points in ascending order
         type(matrix_split_dt), intent(inout) :: split
 
         ! -------------------------------------------------------------------
-        integer(wi) nmax, m, mmax, p, pmax
+        integer(wi) nmax, m, mmax, mmin, p, pmax, p_plus_1, k
 
         real(wp), allocatable :: aloc(:), bloc(:), cloc(:)
         real(wp) delta
@@ -37,44 +51,58 @@ contains
         nmax = size(a)
         mmax = size(points)
 
+        mmin = 1
+        if (split%periodic) mmin = 0
+
         ! splitting data
         if (allocated(split%points)) deallocate (split%points)
-        allocate (split%points(mmax))
-        split%points(:) = points(:)
+        allocate (split%points(0:mmax + 1))
 
-        if (allocated(split%alpha)) deallocate (split%alpha)
-        allocate (split%alpha(2, mmax))
+        split%points(1:mmax) = points(1:mmax)
+        split%points(0) = nmax; split%points(mmax + 1) = nmax      ! extended
+        print *, points(:)
 
         if (allocated(split%z)) deallocate (split%z)
-        allocate (split%z(nmax, mmax))
+        allocate (split%z(nmax, mmin:mmax))
+
+        if (allocated(split%alpha)) deallocate (split%alpha)
+        allocate (split%alpha(2, mmin:mmax))
+
+        if (allocated(split%beta)) deallocate (split%beta)
+        allocate (split%beta(mmax))
+
+        if (allocated(split%gamma)) deallocate (split%gamma)
+        allocate (split%gamma(mmax))
 
         ! -------------------------------------------------------------------
         ! temporary arrays to calculate z
         allocate (aloc(nmax), bloc(nmax), cloc(nmax))
 
-        do m = 1, mmax              ! loop over splitting points
-            p = points(m)           ! index of splitting point
+        do m = mmin, mmax               ! loop over splitting points
+            p = split%points(m)         ! index of splitting point
+            p_plus_1 = mod(p + 1, nmax)
 
             ! Start definition of alpha
-            split%alpha(1, m) = a(p + 1)
+            split%alpha(1, m) = a(p_plus_1)
             split%alpha(2, m) = c(p)
 
-            ! Generate matrix A1
-            b(p) = b(p) - a(p + 1)
-            a(p + 1) = 0.0_wp
-            b(p + 1) = b(p + 1) - c(p)
+            ! Generate matrix Ak
+            b(p) = b(p) - a(p_plus_1)
+            a(p_plus_1) = 0.0_wp
+            b(p_plus_1) = b(p_plus_1) - c(p)
             c(p) = 0.0_wp
 
-            ! Generate vector z
+            ! Generate vector zk
             aloc(:) = a(:); bloc(:) = b(:); cloc(:) = c(:)
             call Thomas3_LU(nmax, aloc, bloc, cloc)
 
             split%z(:, m) = 0.0_wp
-            split%z(p:p + 1, m) = 1.0_wp
+            split%z(p, m) = 1.0_wp
+            split%z(p_plus_1, m) = 1.0_wp
             call Thomas3_Solve(nmax, 1, aloc, bloc, cloc, split%z(:, m))
 
             ! Complete definition of alpha
-            delta = 1.0_wp + split%alpha(1, m)*split%z(p, m) + split%alpha(2, m)*split%z(p + 1, m)
+            delta = 1.0_wp + split%alpha(1, m)*split%z(p, m) + split%alpha(2, m)*split%z(p_plus_1, m)
             if (abs(delta) < small_wp) then
                 call TLab_Write_ASCII(efile, __FILE__//'. Singular matrix M.')
                 call TLab_Stop(DNS_ERROR_THOMAS)
@@ -86,61 +114,103 @@ contains
 
         deallocate (aloc, bloc, cloc)
 
+        ! Calculate beta
+        do m = 1, mmax - 1
+            p = split%points(m)
+            split%beta(m) = split%alpha(1, m)*split%z(p, m + 1) + split%alpha(2, m)*split%z(p + 1, m + 1)
+        end do
+
+        ! Calculate gamma
+        if (split%periodic) then
+            do m = 1, mmax
+                split%gamma(m) = split%alpha(1, 0)*split%z(nmax, m) + split%alpha(2, 0)*split%z(1, m)
+            end do
+        end if
+
         ! -------------------------------------------------------------------
         ! LU decomposition of block matrix Am
-        ! print *, a
-        ! print *, b
-        ! print *, c
-        p = 1
-        do m = 1, mmax
-            pmax = points(m)
+        do k = 1, mmax + 1                  ! loop over blocks
+            p = mod(split%points(k - 1), nmax)
+            p = p + 1
+            pmax = split%points(k)
             call Thomas3_LU(pmax - p + 1, a(p:pmax), b(p:pmax), c(p:pmax))
-            p = pmax + 1
         end do
-        call Thomas3_LU(nmax - p + 1, a(p:nmax), b(p:nmax), c(p:nmax))
 
         return
-    end subroutine Thomas3_Split_Initialize
+    end subroutine Thomas3_Split_Initialize_Global
 
     !########################################################################
     !########################################################################
-    subroutine Thomas3_Split_Solve(a, b, c, split, f, wrk)
+    subroutine Thomas3_Split_Solve_Global(a, b, c, split, f)
         real(wp), intent(in) :: a(:), b(:), c(:)
         type(matrix_split_dt), intent(in) :: split
         real(wp), intent(inout) :: f(:, :)          ! forcing and solution
-        real(wp), intent(inout) :: wrk(:)
 
-        integer(wi) n, nmax, m, mmax, p, pmax, len
+        integer(wi) n, nmax, m, mmin, mmax, p, pmax, p_plus_1, k, len
+        real(wp), allocatable :: xp(:), alpha(:, :)
 
         !########################################################################
         len = size(f, 1)
         nmax = size(f, 2)
-        mmax = size(split%points)
+        mmax = size(split%points) - 2
+
+        allocate (xp(len))
+        allocate (alpha(len, 0:mmax))
 
         ! -------------------------------------------------------------------
-        ! Solving block system Am
-        p = 1
-        do m = 1, mmax
-            pmax = split%points(m)
+        ! Solving block system Am in m+1 block
+        do k = 1, mmax + 1                  ! loop over blocks
+            p = mod(split%points(k - 1), nmax)
+            p = p + 1
+            pmax = split%points(k)
             call Thomas3_Solve(pmax - p + 1, len, &
                                a(p:pmax), b(p:pmax), c(p:pmax), f(:, p:pmax))
-            p = pmax + 1
         end do
-        call Thomas3_Solve(nmax - p + 1, len, a(p:nmax), b(p:nmax), c(p:nmax), f(:, p:nmax))
 
         ! -------------------------------------------------------------------
-        ! Obtain x
-        m = mmax
-        p = split%points(m)
-        wrk(:) = split%alpha(1, m)*f(:, p) + split%alpha(2, m)*f(:, p + 1)
-        do n = 1, nmax
-            f(:, n) = f(:, n) + wrk(:)*split%z(n, m)
+        ! Collect solution at block boundaries and calculate alphas
+        ! written in terms of blocks to mimic parallel computing
+        do k = 1, mmax + 1
+            m = mod(k, mmax + 1)            ! block k calculates alpha_k
+            !                                 block m+1 calculates alpha_0 for circulant case
+            p = split%points(m)
+            p_plus_1 = mod(p + 1, nmax)
+            xp(:) = f(:, p_plus_1)          ! this info comes from block k+1
+            alpha(:, m) = split%alpha(1, m)*f(:, p) + split%alpha(2, m)*xp(:)
         end do
 
-        ! do m = mmax - 1, 1, -1
-        ! end do
+        do k = mmax, 1, -1
+            m = k                           ! block k calculates alpha k
+            if (m == mmax) cycle            ! the last alpha has no beta correction
+            alpha(:, m) = alpha(:, m) + split%beta(m)*alpha(:, m + 1)
+        end do
+
+        if (split%periodic) then            ! block m+1 handles circulant case
+            do m = 1, mmax
+                alpha(:, 0) = alpha(:, 0) + split%gamma(m)*alpha(:, m)
+            end do
+        end if
+
+        ! -------------------------------------------------------------------
+        ! calculate solution in m+1 blocks
+        mmin = 1
+        if (split%periodic) mmin = 0        ! if circulant, down to alpha_0
+
+        do k = mmax + 1, 1, -1              ! loop over blocks
+            p = mod(split%points(k - 1), nmax)
+            p = p + 1
+            pmax = split%points(k)
+            do m = min(k, mmax), mmin, -1   ! each time I have an alpha, update
+                !                             block k+1 needs same alphas as block k
+                do n = p, pmax
+                    f(:, n) = f(:, n) + alpha(:, m)*split%z(n, m)
+                end do
+            end do
+        end do
+
+        deallocate (xp, alpha)
 
         return
-    end subroutine Thomas3_Split_Solve
+    end subroutine Thomas3_Split_Solve_Global
 
 end module Thomas3_Split
--- src/valid/linear/vlinear.f90
+++ src/valid/linear/vlinear.f90
@@ -2,24 +2,24 @@ program vLinear
     use TLab_Constants, only: wp, wi, BCS_NONE
     use Thomas3
     use Thomas3_Split
-    ! use Thomas5
-    use FDM_MatMul
 
     implicit none
 
     integer(wi), parameter :: len = 1
-    integer(wi), parameter :: nx = 128
+    integer(wi), parameter :: nx = 32
     integer(wi), parameter :: nd = 3
 
     real(wp) :: lhs(nx, nd), lhs_loc(nx, nd)
     real(wp) :: u(len, nx), u_loc(len, nx), f(len, nx)
-    real(wp) :: wrk2d(len)
+    real(wp) :: z(nx), wrk(len)     ! for circulant case
     type(matrix_split_dt) split
 
-    integer(wi) id
+    integer(wi) m, mmax, n
 
     integer :: nseed
     integer, allocatable :: seed(:)
+
+    logical, parameter :: periodic = .false.
 
     ! -------------------------------------------------------------------
     ! random number initialization for reproducibility
@@ -28,8 +28,8 @@ program vLinear
     allocate (seed(nseed))
     ! call random_seed(get=seed)
     ! print *, seed
-    ! seed = 123456789    ! putting arbitrary seed to all elements
-    ! call random_seed(put=seed)
+    seed = 123456789    ! putting arbitrary seed to all elements
+    call random_seed(put=seed)
     ! call random_seed(get=seed)
     ! print *, seed
     deallocate (seed)
@@ -37,22 +37,30 @@ program vLinear
     ! -------------------------------------------------------------------
     ! generate system
     call random_number(lhs)
-    do id = 1, nd               ! 1. upper diagonal is 1; see matmul_3d
-        lhs(:, id) = lhs(:, id)/lhs(:, 3)
-    end do
-    lhs(1, 1) = 0.0_wp
-    lhs(nx, 3) = 0.0_wp
 
     call random_number(u)
-    call MatMul_3d(lhs, u, f, ibc=BCS_NONE)
+
+    n = 1
+    f(:, n) = lhs(n, 2)*u(:, n) + lhs(n, 3)*u(:, n + 1)
+    if (periodic) f(:, n) = f(:, n) + lhs(n, 1)*u(:, nx)
+    do n = 2, nx - 1
+        f(:, n) = lhs(n, 1)*u(:, n - 1) + lhs(n, 2)*u(:, n) + lhs(n, 3)*u(:, n + 1)
+    end do
+    f(:, n) = lhs(n, 1)*u(:, n - 1) + lhs(n, 2)*u(:, n)
+    if (periodic) f(:, n) = f(:, n) + lhs(n, 3)*u(:, 1)
 
     ! -------------------------------------------------------------------
     print *, new_line('a'), 'Standard Thomas algorithm'
 
     lhs_loc = lhs
-    call Thomas3_LU(nx, lhs_loc(:, 1), lhs_loc(:, 2), lhs_loc(:, 3))
     u_loc(:, :) = f(:, :)
-    call Thomas3_Solve(nx, len, lhs_loc(:, 1), lhs_loc(:, 2), lhs_loc(:, 3), u_loc)
+    if (periodic) then
+        call Thomas3C_SMW_LU(lhs_loc(:, 1), lhs_loc(:, 2), lhs_loc(:, 3), z)
+        call Thomas3C_SMW_Solve(lhs_loc(:, 1), lhs_loc(:, 2), lhs_loc(:, 3), z, u_loc, wrk)
+    else
+        call Thomas3_LU(nx, lhs_loc(:, 1), lhs_loc(:, 2), lhs_loc(:, 3))
+        call Thomas3_Solve(nx, len, lhs_loc(:, 1), lhs_loc(:, 2), lhs_loc(:, 3), u_loc)
+    end if
 
     call check(u_loc, u, 'linear.dat')
 
@@ -60,9 +68,13 @@ program vLinear
     print *, new_line('a'), 'Splitting Thomas algorithm'
 
     lhs_loc = lhs
-    call Thomas3_Split_Initialize(lhs_loc(:, 1), lhs_loc(:, 2), lhs_loc(:, 3), [nx/2], split)
     u_loc(:, :) = f(:, :)
-    call Thomas3_Split_Solve(lhs_loc(:, 1), lhs_loc(:, 2), lhs_loc(:, 3), split, u_loc, wrk2d)
+    mmax = 3
+    split%periodic = periodic
+
+    call Thomas3_Split_Initialize_Global(lhs_loc(:, 1), lhs_loc(:, 2), lhs_loc(:, 3), &
+                                  [(m, m=nx/(mmax + 1), nx - 1, nx/(mmax + 1))], split)
+    call Thomas3_Split_Solve_Global(lhs_loc(:, 1), lhs_loc(:, 2), lhs_loc(:, 3), split, u_loc)
 
     call check(u_loc, u, 'linear.dat')
 

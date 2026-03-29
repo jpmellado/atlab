223e42550d5150fa1c771c59d6fd256ac46e351e
Juan Pedro Mellado <mellado@gmx.de>
2026-03-29 17:33:54 +0200

    Update supermuc cmake files

--- config/supermuc.cmake
+++ config/supermuc.cmake
@@ -1,7 +1,7 @@
 if ( NOT BUILD_TYPE )
-  message( WARNING "Setting CMAKE_BUILD_TYPE to default value." )
-  set(BUILD_TYPE PARALLEL)
+  set(BUILD_TYPE "PARALLEL")
 endif()
+message( STATUS  "Build type : " ${BUILD_TYPE} )
 
 if ( NOT HYBRID )
   set(HYBRID FALSE)
@@ -9,60 +9,41 @@ else()
   message(WARNING "Compiling for hybrid openMP/MPI usage")
 endif()
 
-if ( NOT PROFILE )
-  set(PROFILE FALSE)
-endif()
+set(CMAKE_Fortran_FLAGS          " -fpp -nbs -save-temps -heap-arrays -unroll -vec-threshold50 " )
+set(CMAKE_Fortran_FLAGS_RELEASE  " -Ofast -march=skylake-avx512 -axcommon-avx512,SSE4.2 -qopt-streaming-stores=always -qopt-zmm-usage=high -qopt-prefetch")
+# -Ofast = -O3, -ipo, -no-prec-div, -static, -xHost; maybe check -axCORE-AVX2
+set(CMAKE_Fortran_FLAGS_DEBUG    " -g -traceback -debug all ")
 
-if ( ${PROFILE} STREQUAL "TRUE" )
-  set(USER_profile_FLAGS "-g")
-endif()
+set(CMAKE_Fortran_COMPILER ifx)
 
 # compiler for parallel build and hybrid flags
-if ( ${BUILD_TYPE} STREQUAL "PARALLEL" )
-  set(ENV{FC} scorep-mpif90)
-  if ( ${PROFILE} STREQUAL "TRUE" )
-    set(CMAKE_Fortran_COMPILER scorep-mpif90)
-  else ()
-    set(CMAKE_Fortran_COMPILER mpif90)
-  endif()
-
+if (BUILD_TYPE STREQUAL "PARALLEL" )
+  set(MPI_Fortran_COMPILER mpiifx)
+  set(PARALLEL MPI_ONLY)
   add_definitions(-DUSE_MPI -DUSE_MPI_IO -DUSE_ALLTOALL)
 
-  if ( ${HYBRID} STREQUAL "TRUE" )
-    set(USER_omp_FLAGS " -qopenmp " )
+  if (HYBRID STREQUAL "TRUE" )
   endif()
 
-  # compiler for serial build
-else( ${BUILD_TYPE} STREQUAL "SERIAL" )
-  set(ENV{FC} ifort)
-  set(CMAKE_Fortran_COMPILER ifort)
 endif()
 
-#set(USER_Fortran_FLAGS          " -fpp ${USER_profile_FLAGS} -nbs -save-temps -simd-vec-threshold50 -unroll-aggressive ${USER_omp_FLAGS} " )
-set(USER_Fortran_FLAGS          " -fpp ${USER_profile_FLAGS} -O3 -nbs -save-temps -heap-arrays -simd -vec-threshold50 -unroll-aggressive ${USER_omp_FLAGS} " )
-set(USER_Fortran_FLAGS_RELEASE  " -march=skylake-avx512 -axcommon-avx512,SSE4.2 -qopt-prefetch -O3 -ipo" )
-#set(USER_Fortran_FLAGS_RELEASE  " -axCORE-AVX2 -qopt-prefetch -O3 -ipo" )
-set(USER_Fortran_FLAGS_DEBUG    " -g -traceback -debug all ")
-
 if ( NOT CMAKE_BUILD_TYPE )
-  set(CMAKE_BUILD_TYPE RELEASE)
+  set(CMAKE_BUILD_TYPE Release)
 endif()
 
 add_definitions(-DUSE_BLAS -DUSE_MKL)
-set(BLAS_LIB         "-lmkl_intel_lp64 -lmkl_sequential -lmkl_core")
+set(BLAS_LIB 	 "-lmkl_intel_lp64 -lmkl_sequential -lmkl_core")
 set(LIBS         ${BLAS_LIB})
 
 add_definitions(-DUSE_FFTW)
-set(FFTW_INCLUDE_DIR "/dss/dsshome1/lrz/sys/spack/release/21.1.1/opt/skylake_avx512/fftw/3.3.8-intel-ysseeml/include")
-#set(FFTW_LIB         "/gpfs/software/juwels/stages/2018a/software/FFTW/3.3.7-ipsmpi-2018a/lib/libfftw3.a")
-set(FFTW_LIB         $ENV{FFTW_LIB})
+set(FFTW_INCLUDE_DIR "/dss/lrzsys/sys/spack/release/sles15.7/24.6.0/opt/skylake_avx512/fftw/3.3.10-gcc-yac4bby/include")
 set(INCLUDE_DIRS ${INCLUDE_DIRS} ${FFTW_INCLUDE_DIR})
-set(LIBS         ${LIBS}         ${FFTW_LIB})
+set(LIBS         ${LIBS}         $ENV{FFTW_SHLIB})
 
 add_definitions(-DUSE_NETCDF)
-set(NC_INCLUDE_DIR     "/dss/dsshome1/lrz/sys/spack/release/21.1.1/opt/skylake_avx512/netcdf-fortran/4.5.3-intel-k5dpfai/include" "/dss/dsshome1/lrz/sys/spack/release/21.1.1/opt/skylake_avx512/netcdf-c/4.7.4-intel-zhencv3/include")
-set(NC_LIB             $ENV{NETCDF_FORTRAN_SHLIB})
+set(NC_INCLUDE_DIR     "/dss/lrzsys/sys/spack/release/sles15.7/24.6.0/opt/skylake_avx512/netcdf-fortran/4.6.1-oneapi-gp3ic7v/include")
 set(INCLUDE_DIRS ${INCLUDE_DIRS} ${NC_INCLUDE_DIR})
-set(LIBS         ${LIBS}         ${NC_LIB})
+set(LIBS         ${LIBS}         $ENV{NETCDF_FORTRAN_SHLIB})
 
+#message(STATUS "Build definitions : " ${})
 set(GNU_SED "gsed")

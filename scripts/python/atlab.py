20aba529ec98b0d4599a351b575eaf1ee93e6e1c
Juan Pedro Mellado <mellado@gmx.de>
2026-06-28 19:40:27 +0200

    Refactoring python scripts using classes for io of grid and fields

--- scripts/python/atlab.py
+++ scripts/python/atlab.py
@@ -1,79 +1,198 @@
 import struct
+import numpy as np
 
 if __name__ != "__main__":
     from __main__ import etype
 
-# getting grid size from tlab.ini
-def getGridSize(nx=0, ny=0, nz=0):
-    if ( nx == 0 ):
-        for line in open('tlab.ini'):
-            if line.lower().replace(" ","").startswith("imax="):
-                nx = int(line.split("=",1)[1])
-                break
 
-    if ( ny == 0 ):
-        for line in open('tlab.ini'):
-            if line.lower().replace(" ","").startswith("jmax="):
-                ny = int(line.split("=",1)[1])
-                break
+class Field:
+    def __init__(
+        self,
+        nx,
+        ny,
+        nz,
+        filename,
+        sizeofheader=-1,  # read it from header in each file
+        # sizeofheader=0,  # bytes
+        # sizeofheader=36,  # bytes, for gate files
+        # sizeofheader=52,  # bytes, for flow files
+        # sizeofheader=44,  # bytes, for scal files
+        etype="<",  # little-endian
+        # etype=">",  # big endian
+        dtype="f",  # floating-point number, single precision
+        # dtype="f",  # floating-point number, double precision
+        # dtype="B",  # unsigned character, for gate files
+    ):
 
-    if ( nz == 0 ):
-        for line in open('tlab.ini'):
-            if line.lower().replace(" ","").startswith("kmax="):
-                nz = int(line.split("=",1)[1])
-                break
+        self.fin = open(filename, "rb")
 
-    print("Grid size is {}x{}x{}.".format(nx,ny,nz))
+        self.sizeofheader = sizeofheader
+        if sizeofheader == -1:
+            raw = self.fin.read(4)
+            self.sizeofheader = struct.unpack(etype + "{}i".format(1), raw)[0]
 
-    return nx, ny, nz
+        self.dtype = dtype
+        match dtype:
+            case "f":  # signle precision
+                self.sizeofdata = 4  # in bytes
+            case "d":  # double precision
+                self.sizeofdata = 8
+            case "B":  # unsigned character, for gate files
+                self.sizeofdata = 1  # in bytes
+
+        self.etype = etype
+
+        self.nx = nx
+        self.ny = ny
+        self.nz = nz
+        self.sizexy = nx * ny * self.sizeofdata
+        self.sizex = nx * self.sizeofdata
+        # self.sizexz = nx * nz * self.sizeofdata
+        # self.sizeyz = ny * nz * self.sizeofdata
+
+        return
+
+    def readXY(self, plane):
+        self.fin.seek(self.sizeofheader + (plane - 1) * self.sizexy, 0)
+        raw = self.fin.read(self.sizexy)
+        a = np.array(
+            struct.unpack((self.etype + "{}" + self.dtype).format(int(nx * ny)), raw)
+        )
+        a = a.reshape((self.ny, self.nx))
+
+        return a
+
+    def readXZ(self, plane):
+        a = np.empty((self.nx, self.nz))
+        self.fin.seek(self.sizeofheader + (plane - 1) * self.sizex, 0)
+        for k in range(self.nz):
+            raw = self.fin.read(self.sizex)
+            a[k, :] = np.array(
+                struct.unpack((self.etype + "{}" + self.dtype).format(int(nx)), raw)
+            )
+            self.fin.seek((self.ny - 1) * self.sizex, 1)
+
+        return a
+
+
+class Grid:
+    def __init__(
+        self,
+        filename="grid",
+        etype="<",  # little-endian
+        # etype=">",  # big endian
+    ):
+        fin = open(filename, "rb")
+
+        # grid file contains 4 bytes record lengths between records
+        # total header size is 56 bytes
+        fin.seek(4, 0)
+        raw = fin.read(3 * 4)
+        nx, ny, nz = struct.unpack(etype + "{}i".format(3), raw)
+        print("Grid size is {}x{}x{}.".format(nx, ny, nz))
+
+        fin.seek(4 + 4, 1)
+        fin.seek(8 * 3, 1)  # advance over scalex, scaley, scalez
+
+        fin.seek(4 + 4, 1)
+        raw = fin.read(nx * 8)
+        self.x = struct.unpack(self.etype + "{}d".format(nx), raw)
+
+        fin.seek(4 + 4, 1)
+        raw = fin.read(ny * 8)
+        self.y = struct.unpack(self.etype + "{}d".format(ny), raw)
+
+        fin.seek(4 + 4, 1)
+        raw = fin.read(nz * 8)
+        self.z = struct.unpack(self.etype + "{}d".format(nz), raw)
+
+        fin.close()
+
+        # return x, y, z
+
+    def writeNetCDF(filename):
+        print("to be done.")
+
 
 # reading grid file and return n-tuples
-def getGrid(nx, ny, nz, name='grid'):
-    fin = open(name, 'rb')
+def getGrid(nx, ny, nz, filename="grid"):
+    fin = open(filename, "rb")
     #
-    fin.seek(56,0)
-    raw = fin.read(nx*8)
-    x = struct.unpack(etype+'{}d'.format(nx), raw)
+    fin.seek(56, 0)
+    raw = fin.read(nx * 8)
+    x = struct.unpack(etype + "{}d".format(nx), raw)
     #
-    fin.seek(8,1)
-    raw = fin.read(ny*8)
-    y = struct.unpack(etype+'{}d'.format(ny), raw)
+    fin.seek(8, 1)
+    raw = fin.read(ny * 8)
+    y = struct.unpack(etype + "{}d".format(ny), raw)
     #
-    fin.seek(8,1)
-    raw = fin.read(nz*8)
-    z = struct.unpack(etype+'{}d'.format(nz), raw)
+    fin.seek(8, 1)
+    raw = fin.read(nz * 8)
+    z = struct.unpack(etype + "{}d".format(nz), raw)
     #
     fin.close()
 
     return x, y, z
+
+
+# getting grid size from tlab.ini
+def getGridSize(nx=0, ny=0, nz=0, filename="tlab.ini"):
+    if nx == 0:
+        for line in open(filename):
+            if line.lower().replace(" ", "").startswith("imax="):
+                nx = int(line.split("=", 1)[1])
+                break
+
+    if ny == 0:
+        for line in open(filename):
+            if line.lower().replace(" ", "").startswith("jmax="):
+                ny = int(line.split("=", 1)[1])
+                break
+
+    if nz == 0:
+        for line in open(filename):
+            if line.lower().replace(" ", "").startswith("kmax="):
+                nz = int(line.split("=", 1)[1])
+                break
+
+    print("Grid size is {}x{}x{}.".format(nx, ny, nz))
+
+    return nx, ny, nz
+
 
 # writing minimal netcfd file, single precision
 def writeNetCfd(filename, varname, time, x, y, z, field):
     import netCDF4 as nc
 
     # creating netcdf
-    file_dst = nc.Dataset(filename+'.nc', 'w')
+    file_dst = nc.Dataset(filename + ".nc", "w")
 
     # create dimensions
-    file_dst.createDimension('t',None)
-    file_dst.createDimension('x',len(x))
-    file_dst.createDimension('y',len(y))
-    file_dst.createDimension('z',len(z))
+    file_dst.createDimension("t", None)
+    file_dst.createDimension("x", len(x))
+    file_dst.createDimension("y", len(y))
+    file_dst.createDimension("z", len(z))
 
-    # create and write independent variables 
-    t_dst = file_dst.createVariable('t', 'f4', ('t',))
-    x_dst = file_dst.createVariable('x', 'f4', ('x',))
-    y_dst = file_dst.createVariable('y', 'f4', ('y',))
-    z_dst = file_dst.createVariable('z', 'f4', ('z',))
+    # create and write independent variables
+    t_dst = file_dst.createVariable("t", "f4", ("t",))
+    x_dst = file_dst.createVariable("x", "f4", ("x",))
+    y_dst = file_dst.createVariable("y", "f4", ("y",))
+    z_dst = file_dst.createVariable("z", "f4", ("z",))
     t_dst[:] = time
     x_dst[:] = x[:]
     y_dst[:] = y[:]
     z_dst[:] = z[:]
 
     # create and write field
-    var_dst = file_dst.createVariable(varname, 'f4', ('z','y','x',))
-    var_dst[:] = field.reshape((len(z),len(y),len(x)))[:]
+    var_dst = file_dst.createVariable(
+        varname,
+        "f4",
+        (
+            "z",
+            "y",
+            "x",
+        ),
+    )
+    var_dst[:] = field.reshape((len(z), len(y), len(x)))[:]
 
     file_dst.close()
- 
-
--- scripts/python/fields2nc.py
+++ scripts/python/fields2nc.py
@@ -6,10 +6,10 @@ import struct
 import sys
 
 # etype = ">" # big-endian
-etype = "<" # little-endian
+etype = "<"  # little-endian
 
-dtype = "d" # floating-point number
-sizeofdata = 8 # in bytes
+dtype = "d"  # floating-point number
+sizeofdata = 8  # in bytes
 
 # dtype = "f" # floating-point number
 # sizeofdata = 4 # in bytes
@@ -17,79 +17,79 @@ sizeofdata = 8 # in bytes
 # dtype = 'B' # unsigned character, for gate files
 # sizeofdata = 1 # in bytes
 
-nx = 0 # number of points in Ox; if 0, then search tlab.ini
-ny = 0 # number of points in Oy; if 0, then search tlab.ini
-nz = 0 # number of points in Oz; if 0, then search tlab.ini
+nx = 0  # number of points in Ox; if 0, then search tlab.ini
+ny = 0  # number of points in Oy; if 0, then search tlab.ini
+nz = 0  # number of points in Oz; if 0, then search tlab.ini
 
 # do not edit below this line
 
 # getting grid size from tlab.ini, if necessary
-if ( nx == 0 ):
-    for line in open('tlab.ini'):
-        if line.lower().replace(" ","").startswith("imax="):
-            nx = int(line.split("=",1)[1])
+if nx == 0:
+    for line in open("tlab.ini"):
+        if line.lower().replace(" ", "").startswith("imax="):
+            nx = int(line.split("=", 1)[1])
             break
 
-if ( ny == 0 ):
-    for line in open('tlab.ini'):
-        if line.lower().replace(" ","").startswith("jmax="):
-            ny = int(line.split("=",1)[1])
+if ny == 0:
+    for line in open("tlab.ini"):
+        if line.lower().replace(" ", "").startswith("jmax="):
+            ny = int(line.split("=", 1)[1])
             break
 
-if ( nz == 0 ):
-    for line in open('tlab.ini'):
-        if line.lower().replace(" ","").startswith("kmax="):
-            nz = int(line.split("=",1)[1])
+if nz == 0:
+    for line in open("tlab.ini"):
+        if line.lower().replace(" ", "").startswith("kmax="):
+            nz = int(line.split("=", 1)[1])
             break
 
-print("Grid size is {}x{}x{}.".format(nx,ny,nz))
+print("Grid size is {}x{}x{}.".format(nx, ny, nz))
 
 # getting data from stdin
-if ( len(sys.argv) <= 2 ):
+if len(sys.argv) <= 2:
     print("Usage: python $0 [3d,xy,xz,yz] varname list-of-files.")
     quit()
 
-datatype  = sys.argv[1]
+datatype = sys.argv[1]
 varname = sys.argv[2]
 setoffiles = sorted(sys.argv[3:])
 
-if   ( datatype == '3d' ):          # full 3d field
+if datatype == "3d":  # full 3d field
     sizeofmask = 0
-elif ( datatype == 'xy' ):          # plane of constant z
+elif datatype == "xy":  # plane of constant z
     sizeofmask = len(str(nz))
-elif ( datatype == 'xz' ):          # plane of constant y
+elif datatype == "xz":  # plane of constant y
     sizeofmask = len(str(ny))
-elif ( datatype == 'yz' ):          # plane of constant x
+elif datatype == "yz":  # plane of constant x
     sizeofmask = len(str(nx))
 else:
     print("Usage: python $0 [3d,xy,xz,yz] list-of-files.")
     quit()
 
 # read grid information
-fin = open('grid', 'rb')
+fin = open("grid", "rb")
 #
-fin.seek(56,0)
-raw = fin.read(nx*8)
-x = struct.unpack(etype+'{}d'.format(nx), raw)
+fin.seek(56, 0)
+raw = fin.read(nx * 8)
+x = struct.unpack(etype + "{}d".format(nx), raw)
 #
-fin.seek(8,1)
-raw = fin.read(ny*8)
-y = struct.unpack(etype+'{}d'.format(ny), raw)
+fin.seek(8, 1)
+raw = fin.read(ny * 8)
+y = struct.unpack(etype + "{}d".format(ny), raw)
 #
-fin.seek(8,1)
-raw = fin.read(nz*8)
-z = struct.unpack(etype+'{}d'.format(nz), raw)
+fin.seek(8, 1)
+raw = fin.read(nz * 8)
+z = struct.unpack(etype + "{}d".format(nz), raw)
 #
 fin.close()
 
 # crop grid data in case of planes
-if   ( datatype == 'xy' ):
+if datatype == "xy":
     nz = 1
     z = np.array([0.0], dtype=np.float32)
-elif ( datatype == 'xz' ):
+elif datatype == "xz":
     ny = 1
     y = np.array([0.0], dtype=np.float32)
-elif ( datatype == 'yz' ):
+elif datatype == "yz":
     nx = 1
     x = np.array([0.0], dtype=np.float32)
 
@@ -97,36 +97,36 @@ elif ( datatype == 'yz' ):
 for file in setoffiles:
     # reading data
     print("Processing file %s ..." % file)
-    fin = open(file, 'rb')
-    fin.seek(5*4) # read the time
+    fin = open(file, "rb")
+    fin.seek(5 * 4)  # read the time
     raw = fin.read(8)
-    time = struct.unpack(etype+'{}d'.format(1), raw)
-    fin.seek(-nx*ny*nz*sizeofdata, 2) # read the field
+    time = struct.unpack(etype + "{}d".format(1), raw)
+    fin.seek(-nx * ny * nz * sizeofdata, 2)  # read the field
     raw = fin.read()
-    a = np.array(struct.unpack((etype+'{}'+dtype).format(int(nx*ny*nz)), raw))
-    a = a.reshape((nz,ny,nx))
+    a = np.array(struct.unpack((etype + "{}" + dtype).format(int(nx * ny * nz)), raw))
+    a = a.reshape((nz, ny, nx))
     fin.close()
 
     # creating netcdf
-    file_dst = nc.Dataset(file+'.nc', 'w')
+    file_dst = nc.Dataset(file + ".nc", "w")
 
     # create dimensions for destiny nc-file
     file_dst.createDimension("t", None)
-    file_dst.createDimension('x',len(x))
-    file_dst.createDimension('y',len(y))
-    file_dst.createDimension('z',len(z))
+    file_dst.createDimension("x", len(x))
+    file_dst.createDimension("y", len(y))
+    file_dst.createDimension("z", len(z))
 
     # create and write independent variables in destiny nc-file using single precision
     t_dst = file_dst.createVariable("t", "f4", ("t",))
-    x_dst = file_dst.createVariable('x', 'f4', ('x',))
-    y_dst = file_dst.createVariable('y', 'f4', ('y',))
-    z_dst = file_dst.createVariable('z', 'f4', ('z',))
+    x_dst = file_dst.createVariable("x", "f4", ("x",))
+    y_dst = file_dst.createVariable("y", "f4", ("y",))
+    z_dst = file_dst.createVariable("z", "f4", ("z",))
     t_dst[:] = np.array([time])
     x_dst[:] = x[:]
     y_dst[:] = y[:]
     z_dst[:] = z[:]
 
-    var_dst = file_dst.createVariable(varname, 'f4', ('t','z','y','x'))
-    var_dst[0,:,:,:] = a[:,:,:]
+    var_dst = file_dst.createVariable(varname, "f4", ("t", "z", "y", "x"))
+    var_dst[0, :, :, :] = a[:, :, :]
 
     file_dst.close()

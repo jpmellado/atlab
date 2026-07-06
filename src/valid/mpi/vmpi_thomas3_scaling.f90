9d61cf22c77cf23a3548f71cf5963fbef9fb5723
Juan Pedro Mellado <mellado@gmx.de>
2026-07-06 11:19:29 +0200

    Adding transpose algorithm to thomas scaling

--- src/valid/mpi/vmpi_thomas3_scaling.f90
+++ src/valid/mpi/vmpi_thomas3_scaling.f90
@@ -1,43 +1,51 @@
 program vMpi_Thomas3_Scaling
     use TLab_Constants, only: wp, wi
     use mpi_f08
-    use TLabMPI_VARS, only: mpiGrid, ims_err, ims_time_trans
+    use TLabMPI_VARS, only: mpiGrid, xMpi, yMpi, ims_err, ims_time_trans
     use Thomas_Parallel
+    use Thomas_Circulant
+    use TLabMPI_Transpose
     implicit none
-
+    
     integer(wi), parameter :: nd = 3            ! number of diagonals
     ! integer(wi), parameter :: nx = 4096         ! full size of each linear system
     ! integer(wi), parameter :: nlines = 32768    ! number of linear systems to solve, 32*1024
-
+    
     integer(wi), parameter :: nx = 8192         ! full size of each linear system
     integer(wi), parameter :: nlines = 65536    ! number of linear systems to solve, 32*2048
-
-    ! integer(wi), parameter :: nx = 16382        ! full size of each linear system
+    
+    ! integer(wi), parameter :: nx = 16384        ! full size of each linear system
     ! integer(wi), parameter :: nlines = 131072   ! number of linear systems to solve, 32*4096
+    
+    ! integer(wi), parameter :: nx = 32768        ! full size of each linear system
+    ! integer(wi), parameter :: nlines = 262144   ! number of linear systems to solve, 32*8192
 
     real(wp) :: lhs(nx, nd)                     ! Diagonals of system matrix A
     real(wp), allocatable :: u(:, :)            ! numerical solution of A u = f
     real(wp), allocatable :: f(:, :)            ! forcing
-    ! real(wp), allocatable :: u_a(:, :)          ! analytical solution
     real(wp) :: wrk2d(nlines, 2)
-
+    
+    real(wp), allocatable :: u_transposed(:, :)
+    type(tmpi_transpose_y_dt) :: tmpi_trp
+    
     type(thomas_parallel_dt) split_mpi
-
-    integer k, np, it, nxLoc
-
+    type(thomas_circulant_dt) :: thomas_circulant1
+    
+    integer k, np, it, nxLoc, nlinesLoc
+    
     ! integer :: nseed
     ! integer, allocatable :: seed(:)
-
+    
     integer, parameter :: num_iterations = 10   ! Number of iterations to obtain a more representative time
     real(wp) time_loc_1, time_loc_2
-
+    
     ! -------------------------------------------------------------------
     call MPI_INIT(ims_err)
-
+    
     mpiGrid%comm = MPI_COMM_WORLD
     call MPI_COMM_SIZE(mpiGrid%comm, mpiGrid%num_processors, ims_err)
     call MPI_COMM_RANK(mpiGrid%comm, mpiGrid%rank, ims_err)
-
+    
     ! -------------------------------------------------------------------
     ! random number initialization for reproducibility
     ! from https://masuday.github.io/fortran_tutorial/random.html
@@ -50,7 +58,7 @@ program vMpi_Thomas3_Scaling
     ! ! call random_seed(get=seed)
     ! ! print *, seed
     ! deallocate (seed)
-
+    
     ! -------------------------------------------------------------------
     ! Initialize
     ! call random_number(lhs)     ! diagonals in matrix A
@@ -60,24 +68,30 @@ program vMpi_Thomas3_Scaling
     lhs(:, 1) = 1.0_wp/3.0_wp   ! first order derivative
     lhs(:, 2) = 1.0_wp
     lhs(:, 3) = 1.0_wp/3.0_wp
-
-    np = mpiGrid%num_processors     ! for clarity below
-    call split_mpi%initialize(lhs, &
-                              [(k, k=nx/np, nx - nx/np, nx/np)], &
-                              block_id=mpiGrid%rank + 1, &
-                              circulant=.true.)
-    split_mpi%mpi = mpiGrid%mpi_axis_dt
-
+    
     ! -------------------------------------------------------------------
     nxLoc = nx/mpiGrid%num_processors     ! task-local number of grid points along X
-    ! allocate(u_a(nlines, nxLoc))
     allocate (u(nlines, nxLoc))
     allocate (f(nlines, nxLoc))
-
+    
     ! call random_number(f)       ! forcing
     f(:, :) = 1.0_wp            ! forcing
+    
+    if (mpiGrid%rank == 0) then
+        print *, new_line('a'), 'Solving ', nlines, ' systems of size ', nx, ' over ', mpiGrid%num_processors, ' processors.'
+    end if
+    
     ! -------------------------------------------------------------------
-    ! Solve and reduce
+    ! Solve using splitting algorithm
+    np = mpiGrid%num_processors     ! for clarity below
+    call split_mpi%initialize(lhs, &
+    [(k, k=nx/np, nx - nx/np, nx/np)], &
+    block_id=mpiGrid%rank + 1, &
+    circulant=.true.)
+    split_mpi%mpi = mpiGrid%mpi_axis_dt
+    
+    call MPI_BARRIER(MPI_COMM_WORLD, ims_err)
+    
     ims_time_trans = 0.0_wp
     time_loc_1 = MPI_WTIME()
     do it = 1, num_iterations
@@ -87,55 +101,53 @@ program vMpi_Thomas3_Scaling
         call split_mpi%reduce(u, wrk2d(:, 1), wrk2d(:, 2))
     end do
     time_loc_2 = MPI_WTIME()
-
+    
     if (mpiGrid%rank == 0) then
-        print *, 'Solving ', nlines, ' systems of size ', nx, ' over ', mpiGrid%num_processors, ' processors.'
+        print *, new_line('a'), 'Splitting algorithm.'
         print *, 'Elapsed time in processor with rank 0 (seconds): ', time_loc_2 - time_loc_1
         print *, 'Communication time in processor with rank 0 (seconds): ', ims_time_trans
         print *, 'Serial time in processor with rank 0 (seconds): ', time_loc_2 - time_loc_1 - ims_time_trans
     end if
-    ! call check(u_a, u)
-
+    
+    call MPI_BARRIER(MPI_COMM_WORLD, ims_err)
+    
+    ! -------------------------------------------------------------------
+    ! Solve using transpose algorithm
+    nlinesLoc = nlines/mpiGrid%num_processors
+    
+    call thomas_circulant1%initialize(lhs(:, 1:nd))
+    
+    allocate (u_transposed(nlinesLoc, nx))
+    yMpi => mpiGrid%mpi_axis_dt
+    xMpi => mpiGrid%mpi_axis_dt ! I need it for the initialization
+    call TLabMPI_Trp_Initialize()
+    
+    call tmpi_trp%initialize(nxLoc, nlines)
+    
+    ims_time_trans = 0.0_wp
+    time_loc_1 = MPI_WTIME()
+    do it = 1, num_iterations
+        u(:, :) = f(:, :)
+        call tmpi_trp%forward(u(:, 1), u_transposed(:, 1))
+        
+        call thomas_circulant1%solveL(u_transposed)
+        call thomas_circulant1%solveU(u_transposed)
+        call thomas_circulant1%reduce(u_transposed, wrk2d(:, 1))
+        
+        call tmpi_trp%backward(u_transposed(:, 1), u(:, 1))
+    end do
+    time_loc_2 = MPI_WTIME()
+    
+    if (mpiGrid%rank == 0) then
+        print *, new_line('a'), 'Transpose algorithm.'
+        print *, 'Elapsed time in processor with rank 0 (seconds): ', time_loc_2 - time_loc_1
+        print *, 'Communication time in processor with rank 0 (seconds): ', ims_time_trans
+        print *, 'Serial time in processor with rank 0 (seconds): ', time_loc_2 - time_loc_1 - ims_time_trans
+    end if
+    
+    ! -------------------------------------------------------------------
     call MPI_FINALIZE(ims_err)
-
+    
     stop
-
-    ! ###################################################################
-! contains
-!     subroutine check(u, u_ref, name)
-!         real(wp), intent(in) :: u(:, :), u_ref(:, :)
-!         character(len=*), optional :: name
-
-!         real(wp) dummy, error_l2, error_max
-!         integer(wi) i, l
-
-!         if (present(name)) then
-!             open (20, file=name)
-!         end if
-!         error_l2 = 0.0_wp
-!         error_max = 0.0_wp
-!         dummy = 0.0_wp
-!         do i = 1, size(u, 2)
-!             do l = 1, size(u, 1)
-!                 if (present(name)) then
-!                     write (20, 1000) u(l, i), u_ref(l, i), u(l, i) - u_ref(l, i)
-!                 end if
-!                 dummy = dummy + u_ref(l, i)*u_ref(l, i)
-!                 error_l2 = error_l2 + (u_ref(l, i) - u(l, i))**2
-!                 error_max = max(error_max, abs(u_ref(l, i) - u(l, i)))
-!             end do
-!         end do
-!         if (present(name)) then
-!             close (20)
-!         end if
-
-!         write (*, *) 'Solution L2-norm ...........:', sqrt(dummy)/real(nlines, wp)
-!         if (dummy == 0.0_wp) return
-!         write (*, *) 'Relative Error L2-norm .....:', sqrt(error_l2)/sqrt(dummy)
-!         write (*, *) 'Relative Error Linf-norm ...:', error_max/abs(maxval(u_ref))
-
-!         return
-! 1000    format(5(1x, e12.5))
-!     end subroutine check
-
+    
 end program
